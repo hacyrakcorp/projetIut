b127c620c16aece3099e9af3024300d35cfde71a
hacyrak <c.mollusk@gmail.com>
2018-10-06 10:58:36 +0200

    Creation projet
    un projet ionic
    template tabs

---
+++ projetIut/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ReperesPage } from '../pages/reperes/reperes';
+import { ParametrePage } from '../pages/parametre/parametre';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ReperesPage,
+    ParametrePage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ReperesPage,
+    ParametrePage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
