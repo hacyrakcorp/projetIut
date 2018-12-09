fe16b6190a2deb8442c4e9b22d613ce03751bd0b
hacyrak <c.mollusk@gmail.com>
2018-12-09 14:15:49 +0100

    construction page info
    modification d'un repere sans bdd

--- projetIut/src/pages/reperes/reperes.ts
+++ projetIut/src/pages/reperes/reperes.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ModalController } from 'ionic-angular';
 import { RepereInfoPage } from '../repere-info/repere-info';
+import { ToastController } from 'ionic-angular';
+
+ //Test param car console.log ne fonctionne pas
+ import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-reperes',
@@ -9,16 +13,46 @@ import { RepereInfoPage } from '../repere-info/repere-info';
 })
 export class ReperesPage {
   reperes = [
-    ["Repere1",48.862725,2.287592],
-    ["Repere2",43.6723861,4.639733200000023],
-    ["Repere3",43.6723861,4.639733200000023]
+    {id:1,nom:"Repere1",latitude:48.862725,longitude:2.287592},
+    {id:2,nom:"Repere2",latitude:43.6723861,longitude:4.639733200000023},
+    {id:3,nom:"Repere3",latitude:46.6723861,longitude:4.56}
   ];
-  constructor(public navCtrl: NavController, public modalCtrl : ModalController) {
+
+  constructor(public navCtrl: NavController, 
+    public modalCtrl : ModalController,
+    public toastCtrl: ToastController,
+    private alertCtrl: AlertController) {
    
   }
 
-  info() {
-	  let modal = this.modalCtrl.create(RepereInfoPage);
+
+  selectRepere(repere) {
+    let modal = this.modalCtrl.create(RepereInfoPage,
+      { item: repere });
+
+     modal.onDidDismiss((rep) => {
+      
+       if(rep){
+         //this.reperes.push(repere);
+         let exist = false;
+        for (var i = 0; i<this.reperes.length;i++){
+          if (this.reperes[i].id == rep.id){
+            this.reperes[i] = rep;
+            exist = true;
+            break;
+          }
+        }
+        if (exist){
+          const toast = this.toastCtrl.create({
+            message: 'Repère modifié avec succès',
+            duration: 3000,
+            position : 'bottom'
+          });
+          toast.present();
+        }
+        
+       }
+     });
 	  modal.present();
   }
 
